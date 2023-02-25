8a4f2b0f1fa75988f3e0067cfa0053f299bf4773
Mandeep Singh <mandeep88712@gmail.com>
2023-02-25 13:44:37 +0530

    default auto field

--- donation/settings.py
+++ donation/settings.py
@@ -122,6 +122,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
